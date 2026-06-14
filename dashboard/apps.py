76651d5ca385f28bc8e491030240ceec2638a6bb
Sam Bourke <sam.bourke@example.com>
2026-06-14 09:00:06 +0100

    feat(blocks): register signal handlers in AppConfig.ready()

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class DashboardConfig(AppConfig):
-    name = 'dashboard'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "dashboard"
+
+    def ready(self):
+        # Import signal handlers so they get registered
+        from . import signals  # noqa: F401
